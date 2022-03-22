2091c65458fe6efe1ab1c04e5d9043afb1f10665
MrThimote <mrthimote@gmail.com>
2022-03-22 18:25:42 +0100

    master: api: added api route

--- lfainfo22/urls.py
+++ lfainfo22/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import api.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls.urlpatterns)
 ]
