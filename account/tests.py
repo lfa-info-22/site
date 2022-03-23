63e9225ab4f4f13d0e7d0713f386e23774302af1
MrThimote <mrthimote@gmail.com>
2022-03-23 18:01:14 +0100

    master: account: added tests for api login

--- account/tests.py
+++ account/tests.py
@@ -2,6 +2,7 @@
 from lfainfo22.tests import ClientTestCase
 from account.views   import LoginView, LogoutView
 from django.test     import TestCase
+from django.urls     import reverse
 
 class LoginTest(ClientTestCase):
     VIEW = LoginView()
@@ -11,9 +12,10 @@ class LoginTest(ClientTestCase):
     def test_can_login_username(self):
         username, email, password = ('user', 'user@test.com', 'password')
         response = self.anonymous_client.post(self.ROUTE, { 'user': username, 'password': password })
-
+        
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.wsgi_request.user.username, username)
+        self.assertEqual(response.url, '/')
 
         self.anonymous_client.logout()
 
@@ -21,6 +23,7 @@ class LoginTest(ClientTestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.wsgi_request.user.username, username)
+        self.assertEqual(response.url, '/')
 
         self.anonymous_client.logout()
 
@@ -28,7 +31,37 @@ class LoginTest(ClientTestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.wsgi_request.user.username, '')
-    ## TODO add more tests
+        self.assertEqual(response.url, reverse(self.VIEW.LOGIN_REDIRECTOR) + '?next=/')
+    def test_redirect_keyword(self):
+        username, email, password = ('user', 'user@test.com', 'password')
+        NROUTE = self.ROUTE + '?' + self.VIEW.LOGIN_REDIRECTOR__NEXT_GET_ARG + '=' + '/url'
+
+        response = self.anonymous_client.post(NROUTE, { 'user': username, 'password': 'A' })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.wsgi_request.user.username, '')
+        self.assertEqual(response.url, reverse(self.VIEW.LOGIN_REDIRECTOR) + '?next=/url')
+
+        response = self.anonymous_client.post(NROUTE, { 'user': username, 'password': password })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.wsgi_request.user.username, username)
+        self.assertEqual(response.url, '/url')
+    def test_json_resp_keyword(self):
+        username, email, password = ('user', 'user@test.com', 'password')
+        NROUTE = self.ROUTE + '?json_resp=true'
+
+        response = self.anonymous_client.post(NROUTE, { 'user': username, 'password': 'A' })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.wsgi_request.user.username, '')
+        self.assertEqual(response.json(), { 'status': False })
+
+        response = self.anonymous_client.post(NROUTE, { 'user': username, 'password': password })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.wsgi_request.user.username, username)
+        self.assertEqual(response.json(), { 'status': True })
 
 class LogoutTest(ClientTestCase):
     VIEW = LogoutView()
