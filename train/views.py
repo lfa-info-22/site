00de071d760e5826245e34300895fd1eb8223a84
MrThimote <mrthimote@gmail.com>
2022-03-28 08:17:39 +0200

    master: train: started training home

--- train/views.py
+++ train/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from lfainfo22.views import BaseView
 
 class TrainIndexView(BaseView):
-    TEMPLATE_NAME = 'train/base.html'
+    TEMPLATE_NAME = 'train/list/exercice_list.html'
