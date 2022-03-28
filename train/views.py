538a7b07e51d04bfbee04312f7ef82a4d4cb1f0b
MrThimote <mrthimote@gmail.com>
2022-03-28 09:21:08 +0200

    master: train: added properties area

--- train/views.py
+++ train/views.py
@@ -3,3 +3,20 @@ from lfainfo22.views import BaseView
 
 class TrainIndexView(BaseView):
     TEMPLATE_NAME = 'train/list/exercice_list.html'
+
+    def get_context_data(self, request, *args, **kwargs):
+        ctx = super().get_context_data(request, *args, **kwargs)
+
+        ctx['exercices'] = [
+            { 'text':'Text', 'icon': 'home' } for i in range(20)
+        ]
+        ctx['properties'] = {
+            'schedulers': [ 
+                {"type":"text", "text":"Révisions de maths"},
+                {"type":"text", "text":"Révisions de maths"},
+                {"type":"text", "text":"Révisions de maths"},
+                {"type":"link", "text":"Nouveau plan", "url": "/train/schedule", "icon": "create"}
+            ]
+        }
+
+        return ctx
